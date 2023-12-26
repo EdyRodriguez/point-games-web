76edace5662206a214f626889c3b26551b87cc11
Edy Rodriguez <zeronoaz@gmail.com>
2023-12-25 22:33:36 -0600

    fix: changed hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
   <div className="text-center lg:w-2/3 pt-4 w-full">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Visitame en tuish :3</h1>
     </div>
-  <iframe src="https://player.twitch.tv/?channel=edycony&parent=https://point-games-web.vercel.app/&muted=true" autoPlay={true} allowFullScreen={true}  scrolling="no" height="480" width="720"></iframe>
+  <iframe src="https://player.twitch.tv/?channel=edycony&parent=point-games-web.vercel.app/&muted=true" autoPlay={true} allowFullScreen={true}  scrolling="no" height="480" width="720"></iframe>
 
   </div>
 </section>
